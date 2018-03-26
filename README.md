e7fa169fce5bc47e0834c633fa226d1475557a84
Pepe Valdivia <jvaldivia@softweb.pe>
2018-03-26 11:05:53 -0500

    Usando resources/application.conf

---
+++ README.md
@@ -0,0 +1,2 @@
+https://github.com/github/gitignore/blob/master/Global/SBT.gitignore
+https://marcinkubala.wordpress.com/2013/10/09/typesafe-config-hocon/
--- src/main/scala/example/Hello.scala
+++ src/main/scala/example/Hello.scala
@@ -1,7 +1,11 @@
 package example
+import com.typesafe.config.ConfigFactory
 
 object Hello extends Greeting with App {
+  val config = ConfigFactory.load()
+  val answer = config.getString("simple-app.answer") 
   println(greeting)
+  println(answer)
 }
 
 trait Greeting {
